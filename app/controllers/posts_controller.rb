93cab00cc9a707084ac1026aa540adbbe50c1c94
Daryl Rogony <darylrogony@gmail.com>
2022-07-20 23:07:25 +0300

    Complete: handling validation in a model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_post_added
   def show
     post = Post.find(params[:id])
     
@@ -8,9 +9,7 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
   end
 
@@ -18,6 +17,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def invalid_post_added(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
